08b1b9f53663bb19a3a4f49ea281624a1299d176
Krunia101 <karuniaibrahim6@gmail.com>
2025-07-25 09:42:32 +0700

    o

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,72 +6,74 @@ import React from "react";
 export const ChartPage: React.FC = () => {
   const { state } = useSidebar();
   return (
-    <div
-      key={state}
-      className={`p-6 transition-all duration-300 ${
-        state == "collapsed"
-          ? "ml-15 flex items-center justify-between"
-          : "ml-[20rem] flex items-center justify-between"
-      }`}
-    >
-      <div className="flex gap-2.5">
-        <div className="flex flex-col justify-start items-stretch mb-8 flex-wrap gap-2.5">
-          <SaldoCard saldo={130000000} ownerName="Lap Uang" />
-          <FinanceSummaryCard
-            title="Surplus"
-            amount={3500000000}
-            month="Juli"
-            percentage={100}
-            color="purple"
-            logo="dompet_kosong"
-            cardWidth={400}
-            cardHeight={200}
-          />
-        </div>
-        <div className="flex flex-col justify-start items-stretch mb-8 flex-wrap gap-2.5">
-          <div className="flex flex-row justify-start items-stretch flex-wrap gap-2.5">
+    <div className="">
+      <div
+        key={state}
+        className={`transition-all duration-300 ${
+          state == "collapsed"
+            ? "ml-5 flex items-center justify-between"
+            : "ml-[18rem] flex items-center justify-between"
+        }`}
+      >
+        <div className="w-[90%] min-h-56 bg-white ml-16 mt-5 rounded-lg flex flex-row items-start justify-between gap-6 p-6 shadow">
+          <div className="flex flex-col gap-6 flex-1">
+            <SaldoCard saldo={130000000} ownerName="Lap Uang" />
             <FinanceSummaryCard
-              title="Saldo awal bulan ini"
-              amount={100000000}
+              title="Surplus"
+              amount={3500000000}
               month="Juli"
-              percentage={59}
-              color="blue"
-              logo="dompet_masuk"
-              cardWidth={320}
-              cardHeight={200}
-            />
-            <FinanceSummaryCard
-              title="Saldo akhir bulan ini"
-              amount={100000000}
-              month="Juli"
-              percentage={59}
-              color="yellow"
-              logo="dompet_masuk"
-              cardWidth={320}
+              percentage={100}
+              color="purple"
+              logo="dompet_kosong"
+              cardWidth={400}
               cardHeight={200}
             />
           </div>
-          <div className="flex gap-2.5">
-            <FinanceSummaryCard
-              title="Penerimaan bulan ini"
-              amount={10000000}
-              month="Juli"
-              percentage={59}
-              color="green"
-              logo="dompet_masuk"
-              cardWidth={320}
-              cardHeight={200}
-            />
-            <FinanceSummaryCard
-              title="Pengeluaran bulan ini"
-              amount={1000000000}
-              month="Juli"
-              percentage={15}
-              color="red"
-              logo="dompet_kosong"
-              cardWidth={320}
-              cardHeight={200}
-            />
+          <div className="flex flex-col gap-6 flex-1">
+            <div className="flex gap-6">
+              <FinanceSummaryCard
+                title="Saldo awal bulan ini"
+                amount={100000000}
+                month="Juli"
+                percentage={59}
+                color="blue"
+                logo="dompet_masuk"
+                cardWidth={340}
+                cardHeight={200}
+              />
+              <FinanceSummaryCard
+                title="Saldo akhir bulan ini"
+                amount={100000000}
+                month="Juli"
+                percentage={59}
+                color="yellow"
+                logo="dompet_masuk"
+                cardWidth={340}
+                cardHeight={200}
+              />
+            </div>
+            <div className="flex gap-6">
+              <FinanceSummaryCard
+                title="Penerimaan bulan ini"
+                amount={10000000}
+                month="Juli"
+                percentage={59}
+                color="green"
+                logo="dompet_masuk"
+                cardWidth={340}
+                cardHeight={200}
+              />
+              <FinanceSummaryCard
+                title="Pengeluaran bulan ini"
+                amount={1000000000}
+                month="Juli"
+                percentage={15}
+                color="red"
+                logo="dompet_kosong"
+                cardWidth={340}
+                cardHeight={200}
+              />
+            </div>
           </div>
         </div>
       </div>
